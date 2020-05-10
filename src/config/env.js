9351efd5e379e6afe6f735867a85ab0b27d6e38a
Mark Powney <mpowney+github@icloud.com>
2020-05-10 22:01:17 +1000

    API hostname updates

--- src/config/env.js
+++ src/config/env.js
@@ -7,7 +7,8 @@ exports.IS_PROD = true; // process.env.NODE_ENV === "production";
 exports.PUBLIC_ASSETS_URL = exports.IS_PROD ? "/assets/" : "/assets/";
 
 exports.API_BASE = exports.IS_PROD
-    ? "https://redirect-api-ause.azurewebsites.net"
+    ? window.location.hostname == "redirectapiaustraliase.z26.web.core.windows.net" || window.location.hostname == 
+        ? "https://redirect-api-australiasoutheast" : "https://redirect-api-ause.azurewebsites.net"
     : "http://localhost:7071";
 
 exports.DEV_HOST = "localhost";
